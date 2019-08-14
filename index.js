b323acf3b824899422d2dbcf4c194c21e57852fb
gangeyarealprintin <gangeya@realprint.in>
2019-08-14 21:44:39 +0530

    var proxy

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ io.on("connection", client => {
   });
 });
 
-server.listen(3000, () =>
-  console.log("Express server is running on localhost:3000")
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () =>
+  console.log(`Express server is running on localhost: ${PORT}`)
 );
